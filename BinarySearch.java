d2a6916468a181943dcc020a9d58894de82a7acc
Aaditya Singh <aadityasingh9601@gmail.com>
2025-06-26 13:11:43 +0530

    Solved aggresive cows

--- BinarySearch.java
+++ BinarySearch.java
@@ -177,6 +177,69 @@ public class BinarySearch {
         int ans = findNthRoot(625, 6);
         System.out.println(ans);
 
+        import java.util.Arrays;
+        
+        //This is the solution for the problem, Aggresive cows.
+        class Solution {
+    public boolean canWePlace(int[] nums,int k, int dist){
+        //We'll place the first cow at the first stall always.
+        //Last means the stall position at which the last cow is stored.
+        int cows = 1; int last = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+           //Ensure the min distance is maintained between two cows.
+           if(nums[i] - last >= dist){
+              cows++;
+              last = nums[i];
+           }
+        }
+        //If we've placed all the cows with the minimum possible distance, then return true, else false.
+        if( cows >= k ) return true;
+        return false;
+
+    }
+    public int aggressiveCows(int[] nums, int k) {
+       //Sort the stalls array first, so that we can easily check if we can place cows or not.
+       Arrays.sort(nums);
+
+       //We know that there will b min of 2 cows and the max distance possible between them is (max-min)
+       //and the least distance will be 1, i.e. when they're at consecutive stalls placed at distance of 1.
+
+       int min = (int)1e9;
+       int max = 0;
+
+       //Find the max & min from the array first.
+       for(int i=0;i<nums.length;i++){
+         if(nums[i] > max){
+            max = nums[i];
+         }
+         if(nums[i] < min){
+           min = nums[i];
+         }
+       }
+
+      int low = 1;
+      int high = max - min;
+      int ans = 0;
+
+       while(low <= high){
+          int mid = low + (high-low)/2;
+
+          if(canWePlace(nums,k,mid) == true){
+             //It can be our possible answer. Let's now go for a bigger value.
+             ans = mid;
+             low = mid+1;
+           }
+           else{
+             //It can't be our answer, let's try to look for a smaller value now.
+             high = mid - 1;
+           }
+       }
+       return ans;
+    }
+}
+
+
     }
      
 }
